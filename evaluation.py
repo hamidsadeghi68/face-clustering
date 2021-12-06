0bc2536f2202a3ee03c528062aecc139972dcfb1
Hamid Sadeghi <hamid.sadeghi68@gmail.com>
2021-12-06 14:06:24 +0330

    add evaluation.py

--- evaluation.py
+++ evaluation.py
@@ -1,7 +1,7 @@
 from sklearn import metrics
 
 def calculate_f_measure(labels_true, labels_pred, beta=1.0):
-  [[tn, fp], [fn, tp]] = sklearn.metrics.cluster.pair_confusion_matrix(labels_true, labels_pred)
+  [[tn, fp], [fn, tp]] = metrics.cluster.pair_confusion_matrix(labels_true, labels_pred)
   precision = tp/(tp + fp)
   recall = tp/(tp + fn)
   f_measure = (beta*beta + 1)*precision*recall/(beta*beta*precision + recall)
