4a13da1f5408f9497247854a7c95e404af9c03e0
Hamid Sadeghi <hamid.sadeghi68@gmail.com>
2021-12-11 10:23:17 +0330

    add matching file

--- matching.py
+++ matching.py
@@ -1,14 +1,16 @@
-def match(template1, template2, metric='Euclidian'):
-    #assert (template1.size == 512)
-    #assert (template2.size == 512)
+import numpy as np
+
+def match(template1, template2, metric='Euclidean'):
+    # assert (template1.size == 512)
+    # assert (template2.size == 512)
     assert (template1.size == template2.size)
 
-    if metric='Euclidian':
-		# Euclidian distance
-	    diff = np.subtract(template1, template2)
-	    return np.sqrt(np.sum(np.square(diff), 1))
-	elif metric='Cosine'
-		# Cosine Similarity
-		return np.dot(template1, template2.T)
-	else
-		raise Exception("input metric must be Euclidian or Cosine!")
+    if metric == 'Euclidean':
+        # Euclidean distance
+        diff = np.subtract(template1, template2)
+        return np.sqrt(np.sum(np.square(diff), 1))
+    elif metric == 'Cosine':
+        # Cosine Similarity
+        return np.dot(template1, template2.T)
+    else:
+        raise Exception("input metric must be Euclidean or Cosine!")
